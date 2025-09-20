61cfb7a251e66a220e84cd53994bb57438a059fd
Auracole <mayomioviejohn@gmail.com>
2025-09-20 04:00:55 +0100

    Initial commit with backend, frontend, dockerfiles, pipeline

---
+++ backend/src/main/java/com/converge_ng/converge_ng/ConvergeNgApplication.java
@@ -0,0 +1,13 @@
+package com.converge_ng.converge_ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvergeNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvergeNgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/converge_ng/converge_ng/ConvergeNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.converge_ng.converge_ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvergeNgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
